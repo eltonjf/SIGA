4f095ab63e82f8dda74016ad86fce1b6d277dbaa
Elton M. Silva <eltonjf@gmail.com>
2016-12-15 15:19:05 -0200

    First commit with responsive layout, CDI, Hibernate and Primefaces configurated

---
+++ src/main/webapp/resources/js/messagesClientSide.js
@@ -0,0 +1,7 @@
+PrimeFaces.locales['pt_BR'] = {
+    messages: {
+        'javax.faces.component.UIInput.REQUIRED' : '{0} é obrigatório',
+        'javax.faces.validator.LengthValidator.MINIMUM' : '{1} deve ter no máximo {0} caracteres',
+        'primefaces.password.INVALID_MATCH' : 'As Senhas não conferem'
+    }
+}
